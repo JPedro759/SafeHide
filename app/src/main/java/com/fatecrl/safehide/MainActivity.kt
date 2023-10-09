e1111a56de01a74bbe420cb7d7be84ec2426a708
lab54 <lab54@fatec.com>
2023-10-09 20:12:28 -0300

    Secure Folder Interface

--- app/src/main/java/com/fatecrl/safehide/MainActivity.kt
+++ app/src/main/java/com/fatecrl/safehide/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.fatecrl.safehide
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSecureFolder = findViewById<Button>(R.id.buttonSecureFolder)
+
+        buttonSecureFolder.setOnClickListener {
+            val intent = Intent(this, SecureFolderActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/fatecrl/safehide/SecureFolderActivity.kt
@@ -0,0 +1,11 @@
+package com.fatecrl.safehide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecureFolderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_securefolder)
+    }
+}
