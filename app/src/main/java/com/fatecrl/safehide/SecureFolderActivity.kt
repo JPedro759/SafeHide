f0ceda4387bd9ec6b0a75835641b88e55eb43d0b
JPedro759 <joaopedromeloo03@gmail.com>
2023-10-10 00:22:38 -0300

    Button back

--- app/src/main/java/com/fatecrl/safehide/SecureFolderActivity.kt
+++ app/src/main/java/com/fatecrl/safehide/SecureFolderActivity.kt
@@ -1,11 +1,20 @@
 package com.fatecrl.safehide
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecureFolderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_securefolder)
+
+        val buttonBack = findViewById<Button>(R.id.buttonback)
+
+        buttonBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
