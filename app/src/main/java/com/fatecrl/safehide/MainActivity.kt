2768d097c61eba9c30de0324e16cfd1717279b48
JPedro759 <joaopedromeloo03@gmail.com>
2023-10-10 23:56:18 -0300

    Button hide

--- app/src/main/java/com/fatecrl/safehide/MainActivity.kt
+++ app/src/main/java/com/fatecrl/safehide/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.fatecrl.safehide
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imagePicker: ActivityResultLauncher<Intent>
+    private val PICK_IMAGE_REQUEST = 1;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +23,24 @@ class MainActivity : AppCompatActivity() {
         buttonSecureFolder.setOnClickListener {
             val intent = Intent(this, SecureFolderActivity::class.java)
             startActivity(intent)
+        }
+
+        val buttonHide = findViewById<Button>(R.id.buttonHide)
+
+        // Inicialize o imagePicker
+        imagePicker = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                if (data != null) {
+                    val selectedImageUri: Uri? = data.data
+                    // Faça algo com a URI da imagem selecionada, como exibi-la em uma ImageView.
+                }
+            }
+        }
+
+        buttonHide.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            imagePicker.launch(intent)
         }
     }
 }
